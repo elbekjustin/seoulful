dc7c5f522fbf79e6f3158a3c529ebb484ef5bf17
elbekjustin <elbekjumanazarov03@gmail.com>
2024-12-06 05:52:47 +0900

    feat: integration of Authentication and Authorization

--- apps/nestar-api/src/components/auth/auth.service.ts
+++ apps/nestar-api/src/components/auth/auth.service.ts
@@ -3,6 +3,7 @@ import * as bcrypt from 'bcryptjs';
 import { Member } from '../../libs/dto/member/member';
 import { JwtService } from '@nestjs/jwt';
 import { T } from '../../libs/types/common';
+import { shapeIntoMongoObjectid } from '../../libs/config';
 
 @Injectable()
 export class AuthService {
@@ -29,6 +30,7 @@ export class AuthService {
 
   public async verifyToken(token: string): Promise<Member> {
     const member = await this.jwtService.verifyAsync(token);
+    member._id = shapeIntoMongoObjectid(member._id);
     return member;
   }
 
