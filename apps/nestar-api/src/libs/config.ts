b5249e8b407852b0a40362bb7f7dd688973fc371
elbekjustin <elbekjumanazarov03@gmail.com>
2024-12-09 19:11:53 +0900

    feat: getAllMembersByAdmin and updateMemberByAdmin APIs

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -1,6 +1,7 @@
 import { ObjectId } from 'bson';
 
 export const availableAgentSorts = ['createdAt', 'updatedAt', 'memberLike', 'memberViews', 'memberRank'];
+export const availableMemberSorts = ['createdAt', 'updatedAt', 'memberLike', 'memberViews'];
 
 export const shapeIntoMongoObjectid = (target: any) => {
     return typeof target === 'string' ? new ObjectId(target) : target;
