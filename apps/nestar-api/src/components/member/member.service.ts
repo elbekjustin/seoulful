854e9287d1fabf866a7a3cdb64c848afc62826cf
elbekjustin <elbekjumanazarov03@gmail.com>
2024-12-09 17:26:03 +0900

    fix: modify view module to record visits

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -7,12 +7,16 @@ import { MemberStatus } from '../../libs/enums/member.enum';
 import { Message } from '../../libs/enums/common.enum';
 import { AuthService } from '../auth/auth.service';
 import { MemberUpdate } from '../../libs/dto/member/member.update';
+import { ViewService } from '../view/view.service';
+import { ViewGroup } from '../../libs/enums/view.enum';
+import { T } from '../../libs/types/common';
 
 @Injectable()
 export class MemberService {
     constructor(
         @InjectModel('Member') private readonly  memberModel: Model<Member>,
         private authService: AuthService,
+        private viewService: ViewService,
     ) {}
 
     public async signup(input: MemberInput): Promise<Member> {
@@ -67,21 +71,36 @@ public async updateMember(memberId: ObjectId, input: MemberUpdate): Promise<Memb
   return result;
 }
 
-public async getMember(targetId: ObjectId): Promise<Member> {
-  const search = {
+public async getMember(memberId: ObjectId, targetId: ObjectId): Promise<Member> {
+  const search: T = {
     _id: targetId,
     memberStatus: {
       $in: [MemberStatus.ACTIVE, MemberStatus.BLOCK],
     },
   };
 
-  const targetMember = await this.memberModel.findOne(search).exec();
-  if (!targetMember) {
+  const targetMember = await this.memberModel.findOne(search).lean().exec();
+  if (!targetMember)
     throw new InternalServerErrorException(Message.NO_DATA_FOUND);
-  }
+
+  if (memberId) {
+    const viewInput = {
+      memberId: memberId,
+      viewRefId: targetId,
+      viewGroup: ViewGroup.MEMBER,
+    };
+    const newView = await this.viewService.recordView(viewInput);
+    if (newView) {
+      await this.memberModel.findOneAndUpdate(search, { $inc: { memberViews: 1 } }, { new: true })
+        .exec();
+      targetMember.memberViews++;
+    }
+  } 
 
   return targetMember;
 }
+
+
 
 
 public async getAllMembersByAdmin(): Promise<string> {
@@ -94,3 +113,4 @@ public async updateMemberByAdmin(): Promise<string> {
     
     
 }
+ 
